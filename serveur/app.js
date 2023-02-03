5c3e4311876498b319591d8cbe9c17bbd2b05444
monsieur486 <monsieur486@gmail.com>
2023-02-03 13:22:19 +0100

    first commit

--- serveur/app.js
+++ serveur/app.js
@@ -1,9 +1,11 @@
-var options = {
+const fs = require('fs');
+
+let options = {
     key: fs.readFileSync('/home/prems/certs/privkey.key'),
     cert: fs.readFileSync('/home/prems/certs/fullchain.crt'),
     requestCert: true
 };
-var server = require('https').createServer(options, app);
-var io = require('socket.io').listen(server);
+let server = require('https').createServer(options, app);
+let io = require('socket.io').listen(server);
 server.listen(8080);
 console.log('Server started at port: 8080');
