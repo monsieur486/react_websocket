51f2dbc6d5e95e360ee92363bbeb44ae983d9d9c
monsieur486 <monsieur486@gmail.com>
2023-02-03 14:22:12 +0100

    first commit

--- serveur/app.js
+++ serveur/app.js
@@ -15,7 +15,7 @@ const options = {
 const https_Server = https.createServer(options, app).listen(8080);
 const io = new Server(https_Server, {
     cors: {
-        origin: "*.mr486.com"
+        origin: "tda.mr486.com"
     }
 });
 
