e64d79ea48b50eb7a31ce811bf7644a505798fff
monsieur486 <monsieur486@gmail.com>
2023-02-03 16:33:57 +0100

    first commit

--- client/public/fronted.js
+++ client/public/fronted.js
@@ -3,7 +3,7 @@ const socket = io(URL, { autoConnect: true });
 
 socket.on('new click', (data) => {
     let clics = data.clics;
-    var text = document.getElementById('clics').textContent = clics;
+    document.getElementById('clics').textContent = clics;
     console.log("Message du serveur: " + clics);
 });
 
