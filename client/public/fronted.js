58ce3a70c255b1f5801241a2e09edafafb0222cc
monsieur486 <monsieur486@gmail.com>
2023-02-03 16:01:16 +0100

    first commit

--- client/public/fronted.js
+++ client/public/fronted.js
@@ -2,7 +2,7 @@ const URL = "https://tda.mr486.com:8080";
 const socket = io(URL, { autoConnect: true });
 
 socket.on('new click', (numUsers) => {
-    console.log("Message du serveur: " + numUsers);
+    console.log("Message du serveur: " + numUsers.toString());
 });
 
 function avertir_serveur() {
