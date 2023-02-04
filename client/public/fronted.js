7d06f5c3c34fb6d06429a65582a8d36dfd0fccfd
monsieur486 <monsieur486@gmail.com>
2023-02-04 02:35:21 +0100

    first commit

--- client/public/fronted.js
+++ client/public/fronted.js
@@ -14,7 +14,6 @@ function avertir_serveur() {
 function raz() {
     bootbox.confirm({
         message: 'Mettre le compteur à <b>0</b> ?',
-        className: 'rubberBand animated',
         closeButton: false,
         buttons: {
             cancel: {
