593a6d23300814dac2d399cf82d79e681f942fbc
monsieur486 <monsieur486@gmail.com>
2023-02-07 02:33:23 +0100

    first commit

--- client/public/fronted.js
+++ client/public/fronted.js
@@ -2,6 +2,17 @@ const socket = io(ws_url, { autoConnect: true });
 
 socket.on('new click', (data) => {
     let clics = data.clics;
+    if(clics===0){
+        document.getElementById('boutonMoins').style.visibility = "hidden";
+        document.getElementById('boutonRaz').style.visibility = "hidden";
+    } else {
+        document.getElementById('boutonMoins').style.visibility = "visible";
+    }
+    if(clics>=5){
+        document.getElementById('boutonRaz').style.visibility = "visible";
+    } else {
+        document.getElementById('boutonRaz').style.visibility = "hidden";
+    }
     document.getElementById('clics').textContent = clics;
     console.log("⚡: Nouvelle valeur de clics reçue depuis le serveur: " + clics);
 });
