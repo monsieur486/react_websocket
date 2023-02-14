b3b374b3b8a34790e7a2941d86767db7922a8ae3
monsieur486 <monsieur486@gmail.com>
2023-02-14 12:34:37 +0100

    first commit

--- client/assets/javascript/fronted.js
+++ client/assets/javascript/fronted.js
@@ -1,4 +1,4 @@
-const socket = io( "http://127.0.0.1:3001", { autoConnect: true });
+const socket = io( SOCKET_URL, { autoConnect: true });
 
 socket.on('new click', (data) => {
     let clics = data.clics;
