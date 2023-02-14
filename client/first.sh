9795fbc08e0aae46a0fc098c53809f131993ccf5
monsieur486 <monsieur486@gmail.com>
2023-02-14 18:39:00 +0100

    first commit

---
+++ client/first.sh
@@ -0,0 +1,7 @@
+cp assets/javascript/config.js.dist assets/javascript/config.js
+nano assets/javascript/config.js
+yarn install
+yarn gulp sass
+yarn gulp scripts
+yarn gulp images
+yarn start
---
+++ client/prod.sh
@@ -0,0 +1,6 @@
+git pull
+yarn install
+yarn gulp sass
+yarn gulp scripts
+yarn gulp images
+yarn start
