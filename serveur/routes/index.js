7830e47d309d552e8cdebf1bfe64c7b02f5da302
monsieur486 <monsieur486@gmail.com>
2023-02-26 16:31:06 +0100

    first commit

--- serveur/routes/index.js
+++ serveur/routes/index.js
@@ -5,8 +5,4 @@ router.get("/", (req, res) => {
     res.send({ response: "I am alive" }).status(200);
 });
 
-router.get("/ping", (req, res) => {
-    res.send({ response: "ping" }).status(200);
-});
-
 module.exports = router;
